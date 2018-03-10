f9363cac429f803fb9d59cc30f590b2088ec0304
01shruti <shruti.goyal94@gmail.com>
2018-03-10 12:39:00 +0000

    update

--- bingAdsAutomation/report_fetch.py
+++ bingAdsAutomation/report_fetch.py
@@ -18,8 +18,6 @@ FROM_DATE= dd.date.today() + dd.timedelta(-30)
 def main(authorization_data,accountID,accountName):
 
     try:
-        # You can submit one of the example reports, or build your own.
-
         #report_request=get_budget_summary_report_request()
         report_request=AdGroupPerformanceReport(accountID,accountName)
         #report_request = GoalReport(accountID, accountName)
@@ -30,6 +28,6 @@ def main(authorization_data,accountID,accountName):
             report_request=report_request,
             result_file_directory = FILE_DIRECTORY, 
             result_file_name = accountName +'-' + str(dd.date.today()) +'.csv',
-            overwrite_result_file = True, # Set this value true if you want to overwrite the same file.
-            timeout_in_milliseconds=TIMEOUT_IN_MILLISECONDS # You may optionally cancel the download after a specified time interval.
+            overwrite_result_file = True, 
+            timeout_in_milliseconds=TIMEOUT_IN_MILLISECONDS 
         )
