5e1d22a8673d1c9b451d7fa6b3d6f91231c5aacf
01shruti <shruti.goyal94@gmail.com>
2018-03-10 12:27:54 +0000

    bing ads

--- bingAdsAutomation/report_fetch.py
+++ bingAdsAutomation/report_fetch.py
@@ -3,3 +3,10 @@ import datetime as dd
 from auth_helper import *
 from output_helper import *
 
+userPath = getpass.getuser() 
+FILE_DIRECTORY='C:/Users/' + userPath + '/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/XXXXXXXXXXXXXXXXX'
+
+DOWNLOAD_FILE_NAME='download1.csv'
+
+REPORT_FILE_FORMAT='Csv'
+
