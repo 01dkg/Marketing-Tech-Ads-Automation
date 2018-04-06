0ac1c320ed833dc4028164e0a7891d61b998a4f3
Shruti <shruti.goyal94@gmail.com>
2018-04-06 10:08:09 +0100

    Changing engine name

--- _db/_storedProcs/sp_CreateTable.sql
+++ _db/_storedProcs/sp_CreateTable.sql
@@ -5,4 +5,4 @@ CREATE TABLE `tbl_keyword` (
     `keyword_user_id` int(11) DEFAULT NULL,
     `keyword_date` datetime DEFAULT NULL,
     PRIMARY KEY (`keyword_id`)
-  ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  )
