5b67e0cb171080e3108774399a9d961857c33def
Deepak <deepak91g@gmail.com>
2018-04-06 10:06:11 +0100

    Updating table defination

--- _db/_storedProcs/sp_CreateTable.sql
+++ _db/_storedProcs/sp_CreateTable.sql
@@ -1,1 +1,8 @@
 CREATE TABLE `tbl_keyword` (
+    `keyword_id` int(11) NOT NULL AUTO_INCREMENT,
+    `keyword_title` varchar(45) DEFAULT NULL,
+    `keyword_description` varchar(5000) DEFAULT NULL,
+    `keyword_user_id` int(11) DEFAULT NULL,
+    `keyword_date` datetime DEFAULT NULL,
+    PRIMARY KEY (`keyword_id`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
