bae68293d4dba3f3a00cca7058f35380aa48ee87
Shruti <shruti.goyal94@gmail.com>
2018-04-07 12:09:59 +0100

    user can add keyword lists directly from interface

--- _db/_storedProcs/sp_addKeyword.sql
+++ _db/_storedProcs/sp_addKeyword.sql
@@ -8,3 +8,19 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_addkeyword`(
     IN p_description varchar(1000),
     IN p_user_id bigint
 )
+BEGIN
+    insert into tbl_keyword(
+        keyword_title,
+        keyword_description,
+        keyword_user_id,
+        keyword_date
+    )
+    values
+    (
+        p_title,
+        p_description,
+        p_user_id,
+        NOW()
+    );
+END$$
+ 
