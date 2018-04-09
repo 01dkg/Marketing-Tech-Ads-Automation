ded0e33f03d72d9da6d2fbbf4e99a2f2594bee26
Shruti <shruti.goyal94@gmail.com>
2018-04-09 09:13:56 +0100

    update

--- _db/_storedProcs/sp_updatekeyword.sql
+++ _db/_storedProcs/sp_updatekeyword.sql
@@ -7,3 +7,12 @@ IN p_file_path varchar(200),
 IN p_is_private int,
 IN p_is_done int
 )
+BEGIN
+update tbl_keyword set
+    keyword_title = p_title,
+    keyword_description = p_description,
+    keyword_file_path = p_file_path,
+    keyword_private = p_is_private,
+    keyword_accomplished = p_is_done
+    where keyword_id = p_keyword_id and keyword_user_id = p_user_id;
+END
