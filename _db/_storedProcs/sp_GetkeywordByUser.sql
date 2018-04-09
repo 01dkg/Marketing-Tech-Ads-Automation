7a369cd00912226f300a5c7e3e5ab8b1a6163dec
Shruti <shruti.goyal94@gmail.com>
2018-04-09 09:15:54 +0100

    update select query

--- _db/_storedProcs/sp_GetkeywordByUser.sql
+++ _db/_storedProcs/sp_GetkeywordByUser.sql
@@ -1,2 +1,11 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetkeywordByUser`(
 IN p_user_id bigint,
+IN p_limit int,
+IN p_offset int,
+out p_total bigint
+)
+BEGIN
+     
+    select count(*) into p_total from tbl_keyword where keyword_user_id = p_user_id;
+ 
+    
