5ad0d11976c5da30c46b3f4386cb3068a0d4c2e9
Shruti <shruti.goyal94@gmail.com>
2018-04-09 18:16:13 +0100

    final update

--- _db/_storedProcs/sp_GetkeywordByUser.sql
+++ _db/_storedProcs/sp_GetkeywordByUser.sql
@@ -8,4 +8,8 @@ BEGIN
      
     select count(*) into p_total from tbl_keyword where keyword_user_id = p_user_id;
  
-    
+    SET @t1 = CONCAT( 'select * from tbl_keyword where keyword_user_id = ', p_user_id, ' order by keyword_date desc limit ',p_limit,' offset ',p_offset);
+    PREPARE stmt FROM @t1;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END
