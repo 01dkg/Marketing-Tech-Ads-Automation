30336e9bb77d597cbd4357b61486b98fc0431cf2
Shruti <shruti.goyal94@gmail.com>
2018-04-10 18:26:43 +0100

    adding create table for user roles

--- _db/_query/tableDefination_DDL.sql
+++ _db/_query/tableDefination_DDL.sql
@@ -6,3 +6,13 @@ CREATE TABLE `tbl_status` (
   PRIMARY KEY (`Status_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1
 
+CREATE TABLE `tbl_user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) DEFAULT NULL,
+  `user_username` varchar(45) DEFAULT NULL,
+  `user_password` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1
+
+
+C
